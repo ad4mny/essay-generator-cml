a3efd503ad823865e80295e73f1fd7a9e136d42e
adamz <58021562+ad4mny@users.noreply.github.com>
2021-11-12 11:36:51 +0800

    added login view

--- application/views/LoginView.php
+++ application/views/LoginView.php
@@ -1,7 +1,41 @@
 <div class="container">
-    <div class="row position-absolute top-50 start-50 translate-middle w-100">
-        <div class="col text-center">
-            test
+    <div id="alert" class="w-75 position-absolute start-50 translate-middle mt-5" style="z-index: 1; top: 10%;">
+        <?php
+        if ($this->session->tempdata('notice') != NULL) {
+            echo '<div class="alert alert-success border-0 shadow alert-dismissible fade show" role="alert">';
+            echo '<i class="fas fa-info-circle fa-fw"></i> ' . $this->session->tempdata('notice');
+            echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+            echo '</div>';
+        }
+        if ($this->session->tempdata('error') != NULL) {
+            echo '<div class="alert alert-danger border-0 shadow alert-dismissible fade show" role="alert">';
+            echo '<i class="fas fa-exclamation-circle fa-fw"></i> ' . $this->session->tempdata('error');
+            echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+            echo '</div>';
+        }
+        ?>
+    </div>
+    <div class="row d-flex vh-100 align-items-center justify-content-center">
+        <div class="col-5">
+            <div class="rounded-3 bg-white shadow p-5">
+                <form class="row g-2" method="post" action="<?php echo base_url(); ?>login">
+                    <div class="col-auto">
+                        <h1 class="display-4" style="color: #104547;">EGCML APP</h1>
+                    </div>
+                    <div class="col-auto">
+                        <h4 class="" style="color: #727072;">Essay Generator</h4>
+                    </div>
+                    <div class="col-auto">
+                        <input type="text" class="form-control" name="username" placeholder="Username" required>
+                    </div>
+                    <div class="col-auto">
+                        <input type="password" class="form-control" name="password" placeholder="Password" required>
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" class="form-control btn btn-primary" name="submit">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
