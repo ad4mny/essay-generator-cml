a6fd5c7ee15324a25cacfe29ce2095bbd9f94ae3
Adam Zulkornain <adamzulkornain00@gmail.com>
2023-05-19 02:54:20 +0800

    update ui for navigation sidebar

--- application/views/templates/Navigation.php
+++ application/views/templates/Navigation.php
@@ -16,23 +16,37 @@
     ?>
 </div>
 
-<!-- Sidebar Menu -->
-<nav id="sidebar">
-    <div class="sidebar-header d-flex flex-row">
-        <h3 class="ml-3 mb-0">EGCML</h3>
+<div class="">
+    <!-- Sidebar Menu -->
+    <div class="d-flex flex-column flex-shrink-0 p-3 bg-white vh-100 shadow" style="width: 280px;">
+        <div class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <img src="<?php echo base_url(); ?>assets/logo.jpeg" width="245px">
+        </div>
+        <hr>
+        <ul class="nav nav-pills flex-column mb-auto">
+            <li class="nav-item">
+                <a href="<?php echo base_url(); ?>dashboard" class="nav-link <?php if ($this->uri->segment(1) == 'dashboard') echo 'active'; ?>">
+                    <i class="isax isax-element-equal mr-2"></i> Group/Team
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="<?php echo base_url(); ?>vocabulary" class="nav-link <?php if ($this->uri->segment(1) == 'vocabulary') echo 'active'; ?>">
+                    <i class="isax isax-book mr-2"></i> Vocabulary
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="<?php echo base_url(); ?>submission" class="nav-link <?php if ($this->uri->segment(1) == 'submission') echo 'active'; ?>">
+                    <i class="isax isax-direct-inbox mr-2"></i> Submission
+                </a>
+            </li>
+        </ul>
+        <hr>
+        <div class="dropdown">
+            <a href="#" class="d-flex align-items-center text-decoration-none">
+                <a class="dropdown-item iconsax text-danger" href="<?php echo base_url(); ?>logout">
+                    <i class="isax isax-logout mr-2"></i> Logout
+                </a>
+            </a>
+        </div>
     </div>
-    <ul class="list-unstyled components">
-        <li class="<?php if ($this->uri->segment(1) == 'dashboard') echo 'active'; ?>">
-            <a href="<?php echo base_url(); ?>dashboard"><i class="fas fa-columns fa-fw fa-sm m-1"></i> Dashboard</a>
-        </li>
-        <li class="<?php if ($this->uri->segment(1) == 'vocabulary') echo 'active'; ?>">
-            <a href="<?php echo base_url(); ?>vocabulary"><i class="fas fa-underline fa-fw fa-sm m-1"></i> Vocabulary</a>
-        </li>
-        <li class="<?php if ($this->uri->segment(1) == 'submission') echo 'active'; ?>">
-            <a href="<?php echo base_url(); ?>submission"><i class="fas fa-align-left fa-fw fa-sm m-1"></i> Submission</a>
-        </li>
-        <li class="<?php if ($this->uri->segment(1) == 'logout') echo 'active'; ?>">
-            <a href="<?php echo base_url(); ?>logout"><i class="fas fa-sign-out-alt fa-fw fa-sm m-1"></i> Logout</a>
-        </li>
-    </ul>
-</nav>
+</div>
